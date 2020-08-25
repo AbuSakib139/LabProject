3a54cf6106ad89bcbea8bfae1ca7085b7c613c0f
AbuSakib139 <abusakib139@gmail.com>
2020-08-25 14:20:09 +0600

    Commit

---
+++ Adder.java
@@ -0,0 +1,9 @@
+class Adder extends Arithmetic{
+
+		public int add(int a, int b){
+
+			int s= a+b;
+			return s;	
+
+		}
+}
---
+++ Lab.java
@@ -0,0 +1,13 @@
+public class Lab{
+
+	public static void main(String args[]){
+	
+	Adder object = new Adder();
+
+	System.out.println("My superclass is : Arithmetic");
+	System.out.println(object.add(10,32) + " " + object.add(7,6) + " " + object.add(10,10));
+
+
+	}
+
+}
